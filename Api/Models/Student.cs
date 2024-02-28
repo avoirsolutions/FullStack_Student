dc96182d889a343153574b4e769b06810ba258ac
Georges Vautherin <avoirsolutions@outlook.com>
2024-02-28 16:11:56 +0000

    first changes

---
+++ Api/Models/Student.cs
@@ -0,0 +1,18 @@
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.SignalR;
+
+namespace Api.Models
+{
+    public class Student
+    {
+       public int Id { get; set; } 
+
+        [Required]
+       public string Name { get; set; } = string.Empty;
+
+       public string? Address { get; set;}
+       public string? PhoneNumber { get; set;}
+       public string? Email { get; set;}
+
+    }
+}
